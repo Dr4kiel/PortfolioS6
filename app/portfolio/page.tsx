3bcf410527fcc0e2e2cee1ccf3eaa0c76521b4e8
Dr4kiel <tristangautier2003@gmail.com>
2024-02-23 18:53:16 +0100

    MaJ page portfolio menu

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,34 @@
+export default function Page() {
+  return (
+    <main>
+      <div className="h-screen flex-col justify-center flex">
+        <h2 className="title">Portfolio</h2>
+        <div className="bg-white mx-auto text-center rounded w-2/3">
+          Work In Progress
+        </div>
+      </div>
+      <div className="grid grid-cols-2">
+        <a href="/portfolio/but2" className="mx-[10%]">
+          <div className="grid grid-rows-2 justify-center skill-card">
+            <div className="flex flex-col justify-center">
+              BUT 2
+            </div>
+            <p className="p-2 text-justify">
+                Mes compétences de la seconde année sont répertoriés dans les 6 compétences du BUT Informatique.
+            </p>
+          </div>
+        </a>
+        <a href="/portfolio/but3" className="mx-[10%]">
+        <div className="grid grid-rows-2 justify-center skill-card">
+            <div className="flex flex-col justify-center">
+              BUT 3
+            </div>
+            <p className="p-2 text-justify">
+                Mes compétences de la troisième et dernière année sont répertoriés dans 3 compétences du BUT Informatique. (Réaliser - Optimiser - Collaborer)
+            </p>
+          </div>
+        </a>
+      </div>
+    </main>
+  );
+}
