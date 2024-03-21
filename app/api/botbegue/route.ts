589c5a696d2e43f79e8b2fdfc78196b9ab56c7d1
Dr4kiel <tristangautier2003@gmail.com>
2024-03-21 15:51:37 +0100

    update ?

--- app/api/botbegue/route.ts
+++ app/api/botbegue/route.ts
@@ -7,10 +7,11 @@ export const GET = async (req: Request, res: Response) => {
     try {
         // read logs from logs_symbol folder and return the logs
         // make a json object with the logs and return it
-        let files = await fs.promises.readdir(`logs_symbol`);
-        return NextResponse.json({ files }, {
+        // update the file list in each request
+        let files = fs.readdirSync('./logs_symbol');
+        return NextResponse.json({ logs: files }, {
             status: 200,
-        });
+        })
     } catch (err) {
         return NextResponse.json({ message: "Internal Server Error", err }.err, {
             status: 500,
