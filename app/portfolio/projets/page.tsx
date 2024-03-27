419263d2d767a711c6583e5a34e626b5df887f08
tg <T.Gautier@gys.fr>
2024-03-27 14:27:26 +0100

    création maquette projet

--- app/portfolio/projets/page.tsx
+++ app/portfolio/projets/page.tsx
@@ -12,6 +12,18 @@ export default function Page() {
                     <a href="#projet3" className="project-link">Projet 3</a>
                 </div>
             </div>
+            <div id='projet1' className='project bg-blue-500'>
+                <h2 className='project-title'>Projet 1</h2>
+                <div className='project-content'>
+                    <div className='img-wrapper'>
+                        <img src='https://via.placeholder.com/150' alt='placeholder' />
+                    </div>
+                    <div className='text-wrapper'>
+                        <p className='project-description'>Description du projet 1</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
