6d347344b0cb05dcfbd4f1cb8f98e0615999ea5b
tg <T.Gautier@gys.fr>
2024-03-28 11:24:14 +0100

    MaJ Page projet
    ~ Modification CSS

--- app/portfolio/projets/components/Project.tsx
+++ app/portfolio/projets/components/Project.tsx
@@ -4,20 +4,15 @@ import Image from 'next/image';
 
 export default function Project(_props: any) {
 
-    let generateID = () => {
-        // create an ID for the object based on the title => remove spaces and lowercase
-        return _props.title.replace(/\s/g, '').toLowerCase();
-    }
-
     return (
-        <div id={generateID()} className='project bg-blue-500'>
+        <div id={_props.id} className='project'>
             <h2 className='project-title'>{_props.title}</h2>
             <div className='project-content'>
                 <div className='img-wrapper'>
-                    <Image src={_props.img} alt={_props.title} fill={true} className='object-contain' />
+                    <Image src={_props.img} alt={_props.title} fill={true} className='object-scale-down' />
                 </div>
                 <div className='text-wrapper'>
-                    <h3 className='project-description'>{_props.title_desc}</h3>
+                    <h3 className='project-title-description'>{_props.title_desc}</h3>
                     <p className='project-description'>{_props.description}</p>
                 </div>
             </div>
--- app/portfolio/projets/page.tsx
+++ app/portfolio/projets/page.tsx
@@ -33,17 +33,21 @@ export default function Page() {
 
     return (
         <main id='corps'>
-            <div id='project-list' className="bg-blue-500">
-                <h1 id='project-list-title' className="bg-green-500">Projets</h1>
-                <div id='project-link-list' className="bg-red-500">
-                    <a href="#projet1" className="project-link">Projet 1</a>
-                    <a href="#projet2" className="project-link">Projet 2</a>
-                    <a href="#projet3" className="project-link">Projet 3</a>
-                </div>
+            <div id='project-list' className="bg-white">
+                <h1 id='project-list-title'>Projets</h1>
+                <ul id='project-link-list' className="">
+                    {projects.length > 0 ? (projects.map((project: Project) => {
+                        return <li><a href={`#${project.id}`} className="project-link">{project.title}</a></li>
+                    })) :
+                        <div className='loading'>
+                            <h1>Loading ...</h1>
+                            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+                        </div>}
+                </ul>
             </div>
             <div className='projects'>
                 {projects.length > 0 ? (projects.map((project: Project) => {
-                    return <Project title={project.title} title_desc={project.title_desc} description={project.description} img={project.img} />
+                    return <Project id={project.id} title={project.title} title_desc={project.title_desc} description={project.description} img={project.img} />
                 })) :
                     <div className='loading'>
                         <h1>Loading ...</h1>
